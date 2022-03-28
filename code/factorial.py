69223d54b89029f2f7ee4791ef00521a114fcdf0
Stanleyy03840 <89219476+Stanleyy03840@users.noreply.github.com>
2022-03-28 12:56:51 -0700

    Update factorial.py

--- code/factorial.py
+++ code/factorial.py
@@ -1,19 +1,27 @@
-class Factorial:
-	def __call__(self, n):
-		if n == 0:
-			return 1
-		elif n == 1:
-			return 1
-		else:
-			return n * self(n-1)
+class factorial(object):
+    def __init__(self,n):
+        self.n = n
+    def factorial(self, n=None):
+        n = self.n if n is None else n
+        result = 1
+        if self.n == 1:
+            result = 1
+            return result
+        else:
+          for x in range(1,n+1):
+            result*=x
+          return result
 
-def tester():
-    number = int(input("Enter a number to factorial: "))
-    try:
-        yeah = Factorial() 
-        print("Answer:", yeah(number))   
-    except:
-        print("Please enter a positive integer.")   
+def printFact():
+    fac1 = factorial(int(input("What number factorial?")))
+    fac2 = factorial(1)
+    fac3 = factorial(8)
+    fac4 = factorial(3)
+    print(fac1.factorial())
+    print("factorial of 1 is:", fac2.factorial())
+    print("factorial of 8 is:", fac3.factorial())
+    print("factorial of 3 is:", fac4.factorial())
+    
 
 if __name__ == "__main__":
-    tester=()
+    printFact()
