674bb3c41f525fc2f1c5facffe8756c83f40093b
Stanleyy03840 <89219476+Stanleyy03840@users.noreply.github.com>
2022-03-29 14:54:20 -0700

    Create lists.py

---
+++ week1/lists.py
@@ -0,0 +1,19 @@
+InfoDb = []
+InfoDb.append({  
+               "Title": "Jurassic Park",  
+               "Year": "1993",  
+               "Director": "Steven Spielberg",  
+               "Genres":["Sci-fi","Action"]  
+              })  
+InfoDb.append({  
+               "Title": "The Dark Knight",  
+               "Year": "2008",  
+               "Director": "Christopher Nolan",  
+               "Genres":["Action","Crime","Thriller"]  
+              })  
+InfoDb.append({  
+               "Title": "The Godfather",  
+               "Year": "1972",  
+               "Director": "Francis Ford Coppola",  
+               "Genres":["Crime","Drama"]  
+              })  
