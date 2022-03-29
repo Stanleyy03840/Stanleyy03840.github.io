bd27024f66798305f89bc3d421615d3ca3d01c92
Stanleyy03840 <89219476+Stanleyy03840@users.noreply.github.com>
2022-03-29 15:00:21 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,86 +1,141 @@
+from week0.christmastree import printTree
+from week0.ship import printShip
+from week0.swap import swap
+from week0.matrix import print_matr
+from week1.fibonacci import printFibo
+from week1.lists import InfoDb
+from week2.factorial import printFact
+from week1.loops import for_loop,while_loop,recursive_loop
+from week2.gcd import gcdtest
+from week2.palindrome import paltest
+from week3.factor import normal_factor
 
-# Main list of [Prompts, Actions]
-# Two styles are supported to execute abstracted logic
-# 1. file names will be run by exec(open("filename.py").read())
-# 2. function references will be executed directly file.function()
-main_menu = [
-    ["Christmas Tree", "week0/christmastree.py"],
-    ["Ship", "week0/ship.py"],
-]
+def buildMenu(menu):
+    for key,value in menu.items():
+        display = value["display"]
+        print(f"{key} ------ {display}")
+    print("What is your choice? (enter the number value) ")
 
-# Submenu list of [Prompt, Action]
-# Works similarly to main_menu
-sub_menu = [
-    ["Swap", "week0/swap.py"],
-    ["Matrix", "week0/matrix.py"]
-]
+def presentMenu(menu):
+    buildMenu(menu)
+    choice = int(input())
+    while choice not in menu:
+        choice = int(input("Please elect a valid item. "))
+    if (choice) in menu:
+        if menu[choice]["type"] == "func":
+            menu[choice]["exec"]()
 
-# Menu banner is typically defined by menu owner
-border = "=" * 25
-banner = f"\n{border}\nPlease Select An Option\n{border}"
+        else:
+            presentMenu(menu[choice]["exec"])
+mathMenu = {
+    1: {
+        "display":"Fibonacci",
+        "exec": printFibo,
+        "type":"func"
+    },
+    2: {
+        "display": "Factorial",
+        "exec":printFact,
+        "type":"func"
+    },
+    3: {
+        "display": "GCD",
+        "exec":gcdtest,
+        "type":"func"
+    },
+    4: {
+        "display": "Factor",
+        "exec":normal_factor,
+        "type":"func"
+    },
+    5: {
+        "display": "Quit",
+        "exec":quit,
+        "type":"func"
+    }
+}
 
-# def menu
-# using main_menu list:
-# 1. main menu and submenu reference are created [Prompts, Actions]
-# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
-def menu():
-    title = "Function Menu" + banner
-    menu_list = main_menu.copy()
-    menu_list.append(["Tri2 submenu", submenu])
-    buildMenu(title, menu_list)
+hack2Menu = {
+    1: {
+        "display":"For loop",
+        "exec": for_loop,
+        "type":"func"
+    },
+    2: {
+        "display":"While loop",
+        "exec": while_loop,
+        "type":"func"
+    },
+    3: {
+        "display":"Recursive",
+        "exec": recursive_loop,
+        "type":"func"
+    },
+    4: {
+        "display":"Quit program",
+        "exec": quit,
+        "type":"func"
+    },
+}
+drawingMenu = {
+    1: {"display":"Christmas Tree",
+    "exec":printTree,
+    "type":"func"},
+    2: {"display":"Ship",
+    "exec":printShip,
+    "type":"func"},
+    3: {"display":"Matrix",
+    "exec":print_matr,
+    "type":"func"},
+    4: {"display":"Swap",
+    "exec":swap,
+    "type":"func"},
+    5: {
+        "display": "Quit",
+        "exec":quit,
+        "type":"func"
+    }
+}
 
-# def submenu
-# using sub menu list above:
-# sub_menu works similarly to menu()
-def submenu():
-    title = "Function Submenu" + banner
-    buildMenu(title, sub_menu)
+listMenu = {
+    1: {"display":"Lists and Loops",
+        "exec": hack2Menu,
+        "type":"submenu"
+       },
+    2: {
+        "display": "Palindrome",
+        "exec":paltest,
+        "type":"func"
+    },
+    3: {
+        "display": "Quit",
+        "exec":quit,
+        "type":"func"
+    }
+}
 
-def buildMenu(banner, options):
-    # header for menu
-    print(banner)
-    # build a dictionary from options
-    prompts = {0: ["Exit", None]}
-    for op in options:
-        index = len(prompts)
-        prompts[index] = op
-
-    # print menu or dictionary
-    for key, value in prompts.items():
-        print(key, '->', value[0])
-
-    # get user choice
-    choice = input("Type your choice> ")
-
-    # validate choice and run
-    # execute selection
-    # convert to number
-    try:
-        choice = int(choice)
-        if choice == 0:
-            # stop
-            return
-        try:
-            # try as function
-            action = prompts.get(choice)[1]
-            action()
-        except TypeError:
-            try:  # try as playground style
-                exec(open(action).read())
-            except FileNotFoundError:
-                print(f"File not found!: {action}")
-            # end function try
-        # end prompts try
-    except ValueError:
-        # not a number error
-        print(f"Not a number: {choice}")
-    except UnboundLocalError:
-        # traps all other errors
-        print(f"Invalid choice: {choice}")
-    # end validation try
-
-    buildMenu(banner, options)  # recursion, start menu over again
-
+mainMenu = {
+    1: {
+        "display":"Math Menu",
+        "exec": mathMenu,
+        "type":"submenu"
+    },
+    2: {
+        "display":"Drawing Menu",
+        "exec": drawingMenu,
+        "type":"submenu"
+    },
+    3: {
+        "display":"List Menu",
+        "exec": listMenu,
+        "type":"submenu"
+    },
+    4: {
+        "display": "Quit",
+        "exec":quit,
+        "type":"func"
+    }
+}
 
 if __name__ == "__main__":
-    menu()
+  presentMenu(mainMenu)
