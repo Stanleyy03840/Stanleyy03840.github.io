df84bd5f5b9a6e1a6861afc1af3b325e77c7b68b
Stanleyy03840 <89219476+Stanleyy03840@users.noreply.github.com>
2022-03-29 14:51:38 -0700

    Update and rename christmastree.py to ship.py

---
+++ week0/ship.py
@@ -0,0 +1,234 @@
+#prefuncy.py
+import time
+import os
+
+Color34 = "\u001b[34m"
+Color37 = "\u001b[37m"
+
+
+# As you can see, its not very optimal 
+def ship1():
+    print("    |\ ")
+    print("    |/ ")
+    print("\__ |__/ ")
+    print(" \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ship2():
+    print("      |\ ")
+    print("      |/ ")
+    print("  \__ |__/ ")
+    print("   \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship3():
+    print("        |\ ")
+    print("        |/ ")
+    print("    \__ |__/ ")
+    print("     \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship4():
+    print("          |\ ")
+    print("          |/ ")
+    print("      \__ |__/ ")
+    print("       \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship5():
+    print("            |\ ")
+    print("            |/ ")
+    print("        \__ |__/ ")
+    print("         \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ship6():
+    print("              |\ ")
+    print("              |/ ")
+    print("          \__ |__/ ")
+    print("           \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship7():
+    print("                |\ ")
+    print("                |/ ")
+    print("            \__ |__/ ")
+    print("             \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship8():
+    print("                  |\ ")
+    print("                  |/ ")
+    print("              \__ |__/ ")
+    print("               \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ship9():
+    print("                    |\ ")
+    print("                    |/ ")
+    print("                \__ |__/ ")
+    print("                 \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship10():
+    print("                      |\ ")
+    print("                      |/ ")
+    print("                  \__ |__/ ")
+    print("                   \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship11():
+    print("                        |\ ")
+    print("                        |/ ")
+    print("                    \__ |__/ ")
+    print("                     \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ship12():
+    print("                          |\ ")
+    print("                          |/ ")
+    print("                      \__ |__/ ")
+    print("                       \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship13():
+    print("                            |\ ")
+    print("                            |/ ")
+    print("                        \__ |__/ ")
+    print("                         \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship14():
+    print("                              |\ ")
+    print("                              |/ ")
+    print("                          \__ |__/ ")
+    print("                           \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship15():
+    print("                                |\ ")
+    print("                                |/ ")
+    print("                            \__ |__/ ")
+    print("                             \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ship16():
+    print("                                  |\ ")
+    print("                                  |/ ")
+    print("                              \__ |__/ ")
+    print("                               \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship17():
+    print("                                    |\ ")
+    print("                                    |/ ")
+    print("                                \__ |__/ ")
+    print("                                 \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ship18():
+    print("                                      |\ ")
+    print("                                      |/ ")
+    print("                                  \__ |__/ ")
+    print("                                   \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ship19():
+    print("                                        |\ ")
+    print("                                        |/ ")
+    print("                                    \__ |__/ ")
+    print("                                     \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ship20():
+    print("                                          |\ ")
+    print("                                          |/ ")
+    print("                                      \__ |__/ ")
+    print("                                       \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+os.system("clear")
+time.sleep(.1)
+ship1()
+time.sleep(.5)
+os.system("clear")
+ship2()
+time.sleep(.5)
+os.system("clear")
+ship3()
+time.sleep(.5)
+os.system("clear")
+ship4()
+time.sleep(.5)
+os.system("clear")
+ship5()
+time.sleep(.5)
+os.system("clear")
+ship6()
+time.sleep(.5)
+os.system("clear")
+ship7()
+time.sleep(.5)
+os.system("clear")
+ship8()
+time.sleep(.5)
+os.system("clear")
+ship9()
+time.sleep(.5)
+os.system("clear")
+ship10()
+time.sleep(.5)
+os.system("clear")
+ship11()
+time.sleep(.5)
+os.system("clear")
+ship12()
+time.sleep(.5)
+os.system("clear")
+ship13()
+time.sleep(.5)
+os.system("clear")
+ship14()
+time.sleep(.5)
+os.system("clear")
+ship15()
+time.sleep(.5)
+os.system("clear")
+ship16()
+time.sleep(.5)
+os.system("clear")
+ship17()
+time.sleep(.5)
+os.system("clear")
+ship18()
+time.sleep(.5)
+os.system("clear")
+ship19()
+time.sleep(.5)
+os.system("clear")
+ship20()
+time.sleep(.5)
+os.system("clear")
+print("or so you thought...")
+time.sleep(.5)
+os.system("clear")
