d508aab272c0125a1c2bf976188217c5ad28a1f1
TianbinLiu <89219591+TianbinLiu@users.noreply.github.com>
2022-03-31 14:21:37 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -8,6 +8,7 @@ from week2.factorial import printFact
 from week1.loops import for_loop,while_loop,recursive_loop
 from week2.gcd import gcdtest
 from week2.palindrome import paltest
+from week2.prime import findprimes
 
 def buildMenu(menu):
     for key,value in menu.items():
@@ -43,6 +44,11 @@ mathMenu = {
         "type":"func"
     },
     4: {
+        "display": "Prime",
+        "exec":findprimes,
+        "type":"func"
+    },
+    5: {
         "display": "Quit",
         "exec":quit,
         "type":"func"
